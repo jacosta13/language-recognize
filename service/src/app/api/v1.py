50e4a73a35f8135837bcff67213ecf9bc0d10e39
dstilesr <d.stiles.r@hotmail.com>
2021-08-20 19:07:14 -0500

    Added controller function to serve HTML from template

--- service/src/app/api/v1.py
+++ service/src/app/api/v1.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import JSONResponse, HTMLResponse
 
-from ..exceptions import InputValidationErr
 from ..core import handlers
 from .models import RecognizeRequest
+from ..exceptions import InputValidationErr
 
+templates = Jinja2Templates("templates")
 router = APIRouter(prefix="/api/v1")
 
 
@@ -32,3 +34,23 @@ async def identify_lang(req: RecognizeRequest) -> JSONResponse:
             "error_message": str(e)
         }
         return JSONResponse(out, status_code=500)
+
+
+@router.get("/identify", response_class=HTMLResponse)
+def identify_form(request: Request):
+    """
+    Present the form to request an identification from the model.
+    """
+    temp = templates.TemplateResponse(
+        "index.html",
+        {
+            "languages": handlers.LangRecModelHandler.LANGUAGES,
+            "request": request
+        }
+    )
+    return temp
+
+
+@router.post("/identify", response_class=HTMLResponse)
+async def identify_form_model():
+    pass
